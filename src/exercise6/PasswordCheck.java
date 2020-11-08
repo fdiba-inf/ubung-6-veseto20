c546a17f5416b94b75c36c198ec94ac6dbaf071b
veseto20 <vesela_krumova2001@abv.bg>
2020-11-08 23:30:50 +0000

    password

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,29 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        while(true){
+         String password = input.nextLine();
+         boolean valid = true;
+         int digitCount = 0;
+         if (password.length<8){
+           valid = false;
+           continue;
+         } else{
+           for(int i=0; i<password.length; i++){
+            if (Character.isDigit(password.charAt(i))){
+              digitCount++
+            }
+             if (!Character.isLetterOrDigit(password.charAt(i))){
+               valid = false;
+               break;
+             }
+           }
+         } if (digitCount<2){
+           valid = false;
+         } if(valid){
+           break;
+         }
+        }
     }
 }
